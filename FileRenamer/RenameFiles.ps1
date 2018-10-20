057ec98e374181a7c7abc3d9239cc8924a7354a3
m0nk3y <30559873+M0nk3yH4cks@users.noreply.github.com>
2018-10-20 10:25:29 +0200

    Initial commit

---
+++ FileRenamer/RenameFiles.ps1
@@ -0,0 +1,24 @@
+# Var Declaration
+$oldNamesFilePath = ".\FileRenamer\oldNames.txt"
+$newNamesFilePath = ".\FileRenamer\newNames.txt"
+
+# Set the encoding to UTF-8 to handle all the charters
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+########################################
+########################################
+# Assign the list of old names to a file
+$oldNames = ([IO.File]::ReadAllLines($oldNamesFilePath))
+
+# Assign the list of new names to a file
+$newNames = ([IO.File]::ReadAllLines($newNamesFilePath))
+
+# Verify the imported file
+$countOld = $oldNames.split("`n").count
+$countNew = $newNames.split("`n").count
+Write-Output "Imported $countOld old names"
+Write-Output "Imported $countNew new names"
+
+if ($countOld -ne $countNew) {
+    Write-Error No
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# PowerShell Utilities
+
+A set of utilities written for the PowerShell to work on Windows just a little faster.
+
+## File Renamer
+### What is ?
+A PowerShell script to rename easily large amount of files.
+### How to use ?
